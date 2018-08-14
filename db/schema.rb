ae81f17c7060548968627458f738596d9d27b5f6
Chungzilla <chung.jazmine@gmail.com>
2018-08-14 11:37:04 -0400

    merged new colum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_230402) do
+ActiveRecord::Schema.define(version: 2018_08_14_151814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_08_13_230402) do
     t.datetime "created_at"
     t.datetime "edited_at"
     t.bigint "user_id"
+    t.string "content"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
