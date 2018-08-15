b5e12e86aa3fbd783ebefc31c31fbf3265d6b621
Chungzilla <chung.jazmine@gmail.com>
2018-08-15 00:01:26 -0400

    added sign-in route

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/flash'
 
+
 require './models/user.rb'
 require './models/post.rb'
 require './models/tag.rb'
@@ -24,10 +25,47 @@ get "/sign-up" do
 end
 
 post "/sign-up" do
-  
+    @user = User.create(
+        first_name: params[:first_name],
+        last_name: params[:last_name],
+        username: params[:username],
+        email: params[:email],
+        password: params[:password],
+        birthday: params[:brithday]
+    )
+
+    #Sign User In
+    session[:user_id] = @user.id
+
+    #Notify User of sign-up was successful
+    flash[:nfo] = "Congrats, you have signed up! You are being taken to your homepage now!"
+
+    #Send User to homepage
+    redirect '/'
 end
 
+#Render Sign-In form
 get "/sign-in" do
-    erb :signed_in_homepage
+    erb :sign_in
+end
+
+#Responds to Sign-In form
+post '/sign-in' do
+    @user = User.find_by(username: params[:username])
+
+    #Check if user exists and if password matches wat is in databse
+    if @user && @user.password == params[:password]
+
+        #Sign the user in
+        session[:userd_id] = @user.id
+
+        #Confir to User that they are signed in
+        flash[:info] = "You are signed in!"
+    else
+        flash[:warning] = "Your Username and/or Password is incorrect"
+
+        #Send user to sign-in again 
+        redirect '/sign-in'
+    end
 end
 
--- models/user.rb
+++ models/user.rb
@@ -3,8 +3,9 @@ class User < ActiveRecord::Base
     validates_presence_of :first_name
     validates_presence_of :last_name
     validates_presence_of :username
-    validates_uniqueness_of :username, {case_sensitive: false, message: "Account already exists with this username. Welcome back, please login!"}
-    validates_uniqueness_of :email, {case_sensitive: false, message: "Account already exists with this email. Welcome back, please login!"}
+    validates :username, uniqueness: true
+    validates_presence_of :email
+    validates :email, uniqueness: true
     validates_presence_of :password
     validates_presence_of :birthday
 end
