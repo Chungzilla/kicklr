34ecc23ea72fa506e0ec7dd4a9ed172ae027b1da
Chungzilla <chung.jazmine@gmail.com>
2018-08-16 23:46:48 -0400

    all styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
-# User.create(first_name: 'Bobby', last_name: 'Booshay', username: 'boozilla', email: 'b.booshay@gmail.com', password: '1234', birthday: 19910722)
+User.create(first_name: 'Bobby', last_name: 'Booshay', username: 'boozilla', email: 'b.booshay@gmail.com', password: '1234', birthday: 19910722)
 
-# User.create(first_name: 'John', last_name: 'Murphy', username: 'LilMurph', email: 'MightyMurphinCodingRangers@jm.com', password: '1234', birthday: 19910722)
+User.create(first_name: 'John', last_name: 'Murphy', username: 'LilMurph', email: 'MightyMurphinCodingRangers@jm.com', password: '1234', birthday: 19910722)
 
-Post.create(title: 'Post One', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post One', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Two', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Two', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Three', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Three', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Four', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Four', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Five', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Five', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Six', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Six', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Seven', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Seven', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Eight', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Eight', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Nine', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Nine', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post Ten', content: 'This is some content for this post', user_id: 1)
+# Post.create(title: 'Post Ten', content: 'This is some content for this post', user_id: 1)
 
-Post.create(title: 'Post 11', content: 'This is some content for this post', user_id: 2)
+# Post.create(title: 'Post 11', content: 'This is some content for this post', user_id: 2)
 
-Post.create(title: 'Post 12', content: 'This is some content for this post', user_id: 2)
+# Post.create(title: 'Post 12', content: 'This is some content for this post', user_id: 2)
 
-Post.create(title: 'Post 13', content: 'This is some content for this post', user_id: 2)
+# Post.create(title: 'Post 13', content: 'This is some content for this post', user_id: 2)
 
-Post.create(title: 'Post 14', content: 'This is some content for this post', user_id: 2)
+# Post.create(title: 'Post 14', content: 'This is some content for this post', user_id: 2)
 
-Post.create(title: 'Post 15', content: 'This is some content for this post', user_id: 2)
+# Post.create(title: 'Post 15', content: 'This is some content for this post', user_id: 2)
 
